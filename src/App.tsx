a665aaa4eec514a1fbe84ad9211e1e22bfb8f6d2
Tomasz Trzaskoma <przytrzask@gmail.com>
2020-09-13 21:44:34 +0200

    basic Game state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import { useStore } from "./useStore"
+
+import { History } from "./components/History"
+import { GameOver } from "./components/GameOver"
+import { Game } from "./components/Game"
 
 function App() {
+  const { gameStatus } = useStore()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <History />
+      {gameStatus === "pending" ? <Game /> : <GameOver />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/GameOver.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export function GameOver() {
+  return <div>GameOver</div>
+}
